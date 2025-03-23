20f2fdc672dfa3267f778b9adebdc0e8d822b341
Justin Green <justingreen@Justins-MacBook-Air.local>
2025-03-23 12:21:37 -0700

    save

--- src/components/Creations/Creations.jsx
+++ src/components/Creations/Creations.jsx
@@ -22,7 +22,6 @@ const Creations = () => {
         <button className="btn see-all-btn uppercase">See All</button>
         </div>
       </div>
-      <section className="creations-list-section">
       <ul className="creation-items-list" aria-label="Creation Items">
         {creationItems.map(creationItem=>(
           <li key={creationItem.id} className="creation-item">
@@ -36,7 +35,6 @@ const Creations = () => {
           </li>
         ))}
       </ul>
-      </section>
       {/* <div className="mobile"> */}
         <button className="btn see-all-btn uppercase mobile">See All</button>
       {/* </div> */}
